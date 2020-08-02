af5d24ff490db22504da313c993b38241be7b310
Federico Tomassetti <federico@tomassetti.me>
2020-08-02 15:58:55 +0200

    move Transaction to commonMain

--- model-client/src/commonMain/kotlin/org/modelix/model/api/Transaction.kt
+++ model-client/src/commonMain/kotlin/org/modelix/model/api/Transaction.kt
@@ -15,4 +15,37 @@
 
 package org.modelix.model.api
 
-expect abstract class Transaction(branch: IBranch?) : ITransaction
+abstract class Transaction(override val branch: IBranch?) : ITransaction {
+
+    override fun containsNode(nodeId: Long): Boolean {
+        return tree!!.containsNode(nodeId)
+    }
+
+    override fun getConcept(nodeId: Long): IConcept? {
+        return tree!!.getConcept(nodeId)
+    }
+
+    override fun getParent(nodeId: Long): Long {
+        return tree!!.getParent(nodeId)
+    }
+
+    override fun getRole(nodeId: Long): String? {
+        return tree!!.getRole(nodeId)
+    }
+
+    override fun getProperty(nodeId: Long, role: String?): String? {
+        return tree!!.getProperty(nodeId, role)
+    }
+
+    override fun getReferenceTarget(sourceId: Long, role: String?): INodeReference? {
+        return tree!!.getReferenceTarget(sourceId, role)
+    }
+
+    override fun getChildren(parentId: Long, role: String?): Sequence<Long>? {
+        return tree!!.getChildren(parentId, role)
+    }
+
+    override fun getAllChildren(parentId: Long): Sequence<Long>? {
+        return tree!!.getAllChildren(parentId)
+    }
+}
