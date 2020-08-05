d02ebc0d7001733da8e9c189e8c220c3eae7faae
Federico Tomassetti <federico@tomassetti.me>
2020-08-05 17:55:03 +0200

    move LazyValue to commonMain

--- model-client/src/commonMain/kotlin/org/modelix/model/lazy/LazyValue.kt
+++ model-client/src/commonMain/kotlin/org/modelix/model/lazy/LazyValue.kt
@@ -15,4 +15,21 @@
 
 package org.modelix.model.lazy
 
-expect abstract class LazyValue<E>
+import org.modelix.model.IKeyValueStore
+
+abstract class LazyValue<E> {
+    private var store: IKeyValueStore? = null
+    private var hash: String? = null
+    var value: E? = null
+        get() {
+            if (hash != null) {
+                field = deserialize(store!![hash])
+                hash = null
+                store = null
+            }
+            return field
+        }
+        private set
+
+    abstract fun deserialize(input: String?): E
+}
