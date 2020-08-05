5ffb92ce242c08f6529c5aaee5521f73aa6a6c3e
Federico Tomassetti <federico@tomassetti.me>
2020-08-05 17:59:35 +0200

    move LazyLoaded to commonMain

--- model-client/src/commonMain/kotlin/org/modelix/model/lazy/LazyLoaded.kt
+++ model-client/src/commonMain/kotlin/org/modelix/model/lazy/LazyLoaded.kt
@@ -15,4 +15,20 @@
 
 package org.modelix.model.lazy
 
-expect abstract class LazyLoaded<E>(hash: String?, store: IDeserializingKeyValueStore?)
+abstract class LazyLoaded<E>(protected var hash: String?, protected var store: IDeserializingKeyValueStore?) {
+    protected fun init() {
+        if (hash == null) {
+            return
+        }
+        try {
+            val deserialized = store!![hash, org.modelix.model.util.Function { serialized: String? -> deserialize(serialized) }]
+            init(deserialized)
+        } finally {
+            store = null
+            hash = null
+        }
+    }
+
+    protected abstract fun init(data: E?)
+    abstract fun deserialize(serialized: String?): E
+}
