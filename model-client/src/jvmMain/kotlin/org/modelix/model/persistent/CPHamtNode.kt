eaa5b4df621597b7b6bbf7363055e768960a0895
Federico Tomassetti <federico@tomassetti.me>
2020-08-05 18:27:22 +0200

    simplifying CPHamtNode

--- model-client/src/jvmMain/kotlin/org/modelix/model/persistent/CPHamtNode.kt
+++ model-client/src/jvmMain/kotlin/org/modelix/model/persistent/CPHamtNode.kt
@@ -18,7 +18,6 @@ package org.modelix.model.persistent
 import org.modelix.model.persistent.SerializationUtil.intFromHex
 import org.modelix.model.persistent.SerializationUtil.longFromHex
 import java.util.*
-import java.util.function.Function
 import java.util.stream.Collectors
 
 actual abstract class CPHamtNode {
@@ -29,17 +28,22 @@ actual abstract class CPHamtNode {
         @JvmStatic
         fun deserialize(input: String): CPHamtNode {
             val parts = input.split("/").dropLastWhile { it.isEmpty() }.toTypedArray()
-            return if ("L" == parts[0]) {
-                CPHamtLeaf(longFromHex(parts[1]), parts[2])
-            } else if ("I" == parts[0]) {
-                CPHamtInternal(
-                    intFromHex(parts[1]),
-                    Arrays.stream(parts[2].split(",").toTypedArray())
-                        .filter { it: String? -> it != null && it.length > 0 }
-                        .collect(Collectors.toList()).toTypedArray()
-                )
-            } else {
-                throw RuntimeException("Unknown type: " + parts[0] + ", input: " + input)
+            return when {
+                "L" == parts[0] -> {
+                    CPHamtLeaf(longFromHex(parts[1]), parts[2])
+                }
+                "I" == parts[0] -> {
+                    CPHamtInternal(
+                            intFromHex(parts[1]),
+
+                            parts[2].split(",").toTypedArray()
+                                    .filter { it: String? -> it != null && it.isNotEmpty() }
+                                    .toTypedArray()
+                    )
+                }
+                else -> {
+                    throw RuntimeException("Unknown type: " + parts[0] + ", input: " + input)
+                }
             }
         }
     }
