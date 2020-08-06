324334308463ff2aff039e9e213b22d48f6517fe
Federico Tomassetti <federico@tomassetti.me>
2020-08-06 17:07:43 +0200

    move IdGenerator to commonMain

--- model-client/src/commonMain/kotlin/org/modelix/model/client/IdGenerator.kt
+++ model-client/src/commonMain/kotlin/org/modelix/model/client/IdGenerator.kt
@@ -16,5 +16,22 @@
 package org.modelix.model.client
 
 import org.modelix.model.api.IIdGenerator
+import org.modelix.model.util.AtomicLong
 
-expect class IdGenerator(clientId: Int) : IIdGenerator
+class IdGenerator(clientId: Int) : IIdGenerator {
+    protected val idSequence: AtomicLong
+    private val clientId: Long
+    override fun generate(): Long {
+        val id = idSequence.incrementAndGet()
+        if (id ushr 32 != clientId) {
+            throw RuntimeException("End of ID range")
+        }
+        return id
+    }
+
+    init {
+        this.clientId = clientId.toLong()
+        idSequence = AtomicLong(this.clientId shl 32)
+    }
+}
+
