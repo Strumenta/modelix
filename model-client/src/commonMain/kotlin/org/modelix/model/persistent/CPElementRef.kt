fa1e4439f0b856ffd7e2d7493e1eba823071d378
Federico Tomassetti <federico@tomassetti.me>
2020-08-07 12:40:23 +0200

    move CPElementRef to commonMain

--- model-client/src/commonMain/kotlin/org/modelix/model/persistent/CPElementRef.kt
+++ model-client/src/commonMain/kotlin/org/modelix/model/persistent/CPElementRef.kt
@@ -15,4 +15,161 @@
 
 package org.modelix.model.persistent
 
-expect abstract class CPElementRef /*package*/
+import kotlin.jvm.JvmStatic
+
+abstract class CPElementRef /*package*/
+internal constructor() {
+    abstract val isGlobal: Boolean
+    abstract val isLocal: Boolean
+    abstract val elementId: Long
+    abstract val treeId: String?
+
+    private class LocalRef(private val id: Long) : CPElementRef() {
+        override fun toString(): String {
+            return "" + id
+        }
+
+        override val isGlobal: Boolean
+            get() = false
+
+        override val isLocal: Boolean
+            get() = true
+
+        override val elementId: Long
+            get() = id
+
+        override val treeId: String?
+            get() {
+                throw RuntimeException("Local reference")
+            }
+
+        override fun equals(o: Any?): Boolean {
+            if (this === o) {
+                return true
+            }
+            if (o == null || o !is LocalRef) {
+                return false
+            }
+            return id == o.id
+        }
+
+        override fun hashCode(): Int {
+            var result = 0
+            result = 31 * result + (id xor (id shr 32)).toInt()
+            return result
+        }
+    }
+
+    private class GlobalRef(treeId1: String, elementId1: Long) : CPElementRef() {
+        override val treeId: String?
+        override val elementId: Long
+        override fun toString(): String {
+            return "G$treeId#$elementId"
+        }
+
+        override val isGlobal: Boolean
+            get() = true
+
+        override val isLocal: Boolean
+            get() = false
+
+        override fun equals(o: Any?): Boolean {
+            if (this === o) {
+                return true
+            }
+            if (o == null || o !is GlobalRef) {
+                return false
+            }
+            val that = o
+            if (elementId != that.elementId) {
+                return false
+            }
+            return !if (treeId != null) treeId as Any != that.treeId else that.treeId != null
+        }
+
+        override fun hashCode(): Int {
+            var result = 0
+            result = 31 * result + (elementId xor (elementId shr 32)).toInt()
+            result = 31 * result + (treeId?.hashCode() ?: 0)
+            return result
+        }
+
+        init {
+            treeId = treeId1
+            elementId = elementId1
+        }
+    }
+
+    class MpsRef(ref: String) : CPElementRef() {
+        val serializedRef: String?
+
+        override fun toString(): String {
+            return "M" + serializedRef
+        }
+
+        override val isGlobal: Boolean
+            get() = false
+
+        override val isLocal: Boolean
+            get() = false
+
+        override val elementId: Long
+            get() {
+                throw RuntimeException("MPS reference")
+            }
+
+        override val treeId: String?
+            get() {
+                throw RuntimeException("MPS reference")
+            }
+
+        override fun equals(o: Any?): Boolean {
+            if (this === o) {
+                return true
+            }
+            if (o == null || o !is MpsRef) {
+                return false
+            }
+            val that = o
+            return !if (serializedRef != null) serializedRef as Any != that.serializedRef else that.serializedRef != null
+        }
+
+        override fun hashCode(): Int {
+            var result = 0
+            result = 31 * result + if (serializedRef != null) serializedRef.toString().hashCode() else 0
+            return result
+        }
+
+        init {
+            serializedRef = ref
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun local(elementId: Long): CPElementRef {
+            return LocalRef(elementId)
+        }
+
+        fun global(treeId: String, elementId: Long): CPElementRef {
+            return GlobalRef(treeId, elementId)
+        }
+
+        fun mps(pointer: String): CPElementRef {
+            return MpsRef(pointer)
+        }
+
+        @JvmStatic
+        fun fromString(str: String): CPElementRef {
+            return if (str[0] == 'G') {
+                val i = str.lastIndexOf("#")
+                global(str.substring(1, i), str.substring(i + 1).toLong())
+            } else if (str[0] == 'M') {
+                mps(str.substring(1))
+            } else {
+                local(str.toLong())
+            }
+        }
+    }
+}
+
